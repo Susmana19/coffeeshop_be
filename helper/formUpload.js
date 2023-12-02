89f100e6d5dc06b9c2399618c492c7ffb8ef889e
--add <susmanam@gmail.com>
2023-12-02 14:56:56 +0700

    revert remove tmp directory

--- helper/formUpload.js
+++ helper/formUpload.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/uploads/images/tmp')
+    cb(null, 'public/uploads/images')
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname)
